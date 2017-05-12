ec7034606cc6107eac027d20624c165851b03181
sacascio <sacascio@cisco.com>
2017-05-12 12:13:09 -0400

    Check for valid Numbers

--- parse_input.py
+++ parse_input.py
@@ -4,9 +4,18 @@ import getopt
 import sys
 import subprocess
 import os
+import openpyxl
+from openpyxl.workbook import workbook
+from argparse import Namespace
 
 
-# Script options
+
+# Prepared by Salvatore Cascio, Cisco Systems
+# May 12, 2017
+
+# Script assumptions:
+#    1. Input file must be xls or xlsx
+#    2. Input data must be the first worksheet
 
 
 # Open file if exists
@@ -15,11 +24,23 @@ import os
 # Read each cell and write to new input file
 
 
+def isNum(*args):
+    allfailed = 0
+    all = args
+    for a in all:
+        isvnum = a.isnumeric()
+        if isvnum == False:
+            print (a + " is not a valid Number" )
+            allfailed = 1
+        
+
+    return allfailed
+  
 
 def main(argv):
     
     try:
-        opts,args = getopt.getopt(argv,"f:",["file="])
+        opts,args = getopt.getopt(argv,"hf:",["file="])
     except getopt.GetoptError as err:
         print str(err)
         sys.exit(2)
@@ -43,6 +64,42 @@ def main(argv):
     except NameError:
         print "Filename not specified (-f or --file)"
         sys.exit(1)
+        
+        
+    
+    if not os.path.isfile(filename):
+        print('Filename ' + filename + " does not exist")
+        sys.exit(4)
+        
+    workbook = openpyxl.load_workbook(filename)
+    
+    sessionsheet = workbook.worksheets[0]
+    
+    for row in range (1, sessionsheet.max_row+1):
+        sessionid,macsessid,gwname,gwip,input_gbe,tsid,frequency,inudp,outudp,inmpeg,outmpeg,bw,ingbeip,outgbeip,pk,nds,nagra,sn,modulation,inpid,outpid,type,pmt,pcr = (sessionsheet['A' + str(row)].value).split("|")
+        validnums   = isNum(sessionid,input_gbe,tsid,frequency,inudp,outudp,inmpeg,outmpeg,bw,pk,nds,nagra,inpid,outpid,pmt,pcr)
+        
+        if validnums == 1:
+            print('Row %s has failures' % row)
+            print('****************************\n\n')
+        
+        """
+        validsessid = isValidSessId(macsessid)
+        validip     = isValidIP(gwip,ingbeip,outgbeip)
+        validMod    = isValidMod(modulation)
+        validtype   = isValidType(type)
+        
+        
+        if validnums & validsessid & validip & validMod & validtype:
+            #Write to log file
+        """
+        
+    
+    
+    
+    
+        
+    
     
     
 if __name__ == '__main__':
