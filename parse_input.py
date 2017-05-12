9a5602abf26bc432b16120fb15cb1be78acfaf0a
sacascio <sacascio@cisco.com>
2017-05-12 10:14:56 -0400

    Draft 1

--- parse_input.py
+++ parse_input.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+import getopt
+import sys
+import subprocess
+import os
+
+
 # Script options
 
 
@@ -7,3 +13,37 @@
 
 
 # Read each cell and write to new input file
+
+
+
+def main(argv):
+    
+    try:
+        opts,args = getopt.getopt(argv,"f:",["file="])
+    except getopt.GetoptError as err:
+        print str(err)
+        sys.exit(2)
+    else:
+        for opt,arg in opts:
+            if opt == '-h':
+                print sys.argv[0] + " -f|--file <file_name>"
+                sys.exit(1)
+            elif opt in ( "-f", "--file"):
+                filename = arg
+            else:
+                assert False, "Unknown"
+                sys.exit(2)
+
+    if len(argv) == 0:
+        print "Usage: " +  sys.argv[0] + " -f|--file <file_name> No arguments given"
+        sys.exit(1)
+
+    try:
+        filename
+    except NameError:
+        print "Filename not specified (-f or --file)"
+        sys.exit(1)
+    
+    
+if __name__ == '__main__':
+    main(sys.argv[1:])
