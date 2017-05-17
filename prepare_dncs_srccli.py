f1e4b29937ed57e5969f70cf2fedcc0355879997
Cascio, Salvatore (sacascio) <sacascio@cisco.com>
2017-05-17 11:40:30 -0400

    Version 2

--- prepare_dncs_srccli.py
+++ prepare_dncs_srccli.py
@@ -6,62 +6,74 @@ import sys
 import os
 
 # Prepared by Salvatore Cascio, Cisco Systems
-# May 16, 2017
+# May 17, 2017
+# This script takes the prepared LSM File, pulls out the unique MAC and Session ID, and finds the srcCli equivalent in the prepared srcCli data that was run by getCFsessions.pl
+# Pre requisites:
+#	1. LSM input file is prepared.  Used with option l|--lsm
+#	2. Dump srcCli session script (getCFsessions.pl) is prepared. Used with option d|--dncs
 
-
-
-def getpidtype(pidtype):
-	if pidtype == 'Video':
-		return 1
-	if pidtype == 'Audio':
-		return 81
-	if pidtype == 'Private':
-		return 5
+def loadsrccli(dncsfilename):
+	srcCli = {}
+	with open(dncsfilename,"r") as f:
+		for line in f:
+			line       = line.rstrip()	
+			origline   = line
+			line       = line.split(',')
+			kp         = line[0] + line[1]
+			srcCli[kp] = origline
+			
+	return srcCli
 
 def main(argv):
     writelog = 1
     
     try:
-        opts,args = getopt.getopt(argv,"hf:z:",["file=","hub="])
+        opts,args = getopt.getopt(argv,"hl:d:",["lsm=","dncs="])
     except getopt.GetoptError as err:
-        print (istr(err))
+        print (str(err))
         sys.exit(2)
     else:
         for opt,arg in opts:
             if opt == '-h':
-                print (sys.argv[0] + " -f|--file <file_name>")
+                print (sys.argv[0] + " -l|--lsm <lsm_file_name> -d|--dncs <dncs source cli file>")
                 sys.exit(1)
-            elif opt in ( "-f", "--file"):
-                filename = arg
-            elif opt in ( "-z", "--hub"):
-                hubname = arg
+            elif opt in ( "-d", "--dncs"):
+                dncsfilename = arg
+            elif opt in ( "-l", "--lsm"):
+                lsmfilename  = arg
             else:
                 assert False, "Unknown"
                 sys.exit(2)
 
     if len(argv) == 0:
-        print ("Usage: " +  sys.argv[0] + " -f|--file <file_name> -z|--hub <hub_name> No arguments given")
+        print ("Usage: " +  sys.argv[0] + " -l|--lsm <lsm_file_name> -d|--dncs <dncs source cli file>  No arguments given")
         sys.exit(1)
 
     try:
-        filename
+        dncsfilename 
     except NameError:
-        print ("Filename not specified (-f or --file)")
-        sys.exit(1)
-    
-    try:
-        hubname
-    except NameError:
-        print ("Hub not specified (-z or --hub)")
-        sys.exit(1)
+       	print ("DNCS Filename not specified. Use option -d or --dncs ")
+       	sys.exit(1)
+    else:
+	if not os.path.isfile(dncsfilename):
+		print "File %s not found on the filesystem for DNCS Source CLI" %dncsfilename
+		sys.exit(1)
 
-    sourcemac = {'sm':'NA'}
-    struct = []
+    try:
+        lsmfilename 
+    except NameError:
+       	print ("LSM Filename not specified.  Use option -l or --lsm ")
+       	sys.exit(1)
+    else:
+	if not os.path.isfile(lsmfilename):
+		print "File %s not found on the filesystem for LSM" %lsmfilename
+		sys.exit(1)
     
-    # Sort input file to ensure rows are in order
-    os.system('sort -u %s -o %s' % (filename,filename))
+    sourcemac = {}
     
-    with open(filename) as f:
+    srcclisess = loadsrccli(dncsfilename)
+ 
+    with open(lsmfilename) as f:
 	for x in f:
 		elem = x.split("|")
 		sourceid = elem[0]
@@ -70,61 +82,14 @@ def main(argv):
 		mac      = ":".join(mac[i]+mac[i+1] for i in range (0,12,2))
 		key      = sourceid + mac
 	
-		if  sourcemac['sm'] == key:
-			pidtype  = getpidtype(elem[-4])	
-			inpid    = hex(int(elem[19])).replace('0x','').upper()
-			outpid   = hex(int(elem[20])).replace('0x','').upper()
-			struct.append(inpid)
-			struct.append(outpid)
-			struct.append(pidtype)
+		if  (not sourcemac.has_key(key)):
+			sourcemac[key] = [sourceid,mac]
+
+	for x in sourcemac.keys():
+		if srcclisess.has_key(x):
+			print srcclisess[x]
 		else:
-			if  sourcemac['sm'] != 'NA':
-				print (','.join(map(str, struct))) 
-				struct[:] = []
-			sourcemac = { 'sm' : key }
-			gwname   = elem[2]
-			mpsrc    = elem[17]
-			srmch    = elem[4]
-			inudp    = elem[7].replace('.0','')
-			outudp   = elem[8].replace('.0','')
-			inmpeg   = elem[9]
-			outmpeg  = elem[10]
-			bw       = float(elem[11]) / 1000000
-			if bw % 1 == 0:
-				bw = int(bw)
-			if bw < 1:
-				bw = str(bw)
-				bw = bw.replace('0.','.')
-			pmt      = hex(int(elem[-3])).replace('0x','')
-			pcr      = hex(int(elem[-2])).replace('0x','')
-			pk       = hex(int(elem[14])).replace('0x','').upper()
-			nds      = hex(int(elem[15])).replace('0x','').upper()
-			nagra    = hex(int(elem[16])).replace('0x','').upper()
-			pidtype  = getpidtype(elem[-4])	
-			inpid    = hex(int(elem[19])).replace('0x','').upper()
-			outpid   = hex(int(elem[20])).replace('0x','').upper()
-		
-			struct.append(sourceid)
-			struct.append(mac)
-			struct.append(mpsrc)
-			struct.append(gwname)
-			struct.append(srmch)
-			struct.append(inudp)
-			struct.append(outudp)
-			struct.append(inmpeg)
-			struct.append(outmpeg)
-			struct.append(bw)
-			struct.append(pmt)
-			struct.append(pcr)
-			struct.append(pk)
-			struct.append(nds)
-			struct.append(nagra)
-			struct.append(inpid)
-			struct.append(outpid)
-			struct.append(pidtype)
-	
-	# Print last line of file
-	print (','.join(map(str, struct)))
+			print("WARNING!! NO DNCS SOURCE CLI FOUND FOR SESSION ID %s, MAC ADDRESS %s!!") %(sourcemac[x][0],sourcemac[x][1])
 		
 if __name__ == '__main__':
     main(sys.argv[1:])
